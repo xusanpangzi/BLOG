b0964c6b129ddb8efb7700e21b8efe01fd41106d
xusanpangzi <767306336@qq.com>
2018-07-30 17:49:54 +0800

    third

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9gz94m9kxw5x2eg*z(@j_^o4$*s1457*16&4d@27r#&)f3-uoa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.xn--ehqp73aml3a.xyz']
 
@@ -123,4 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import env,run
+from fabric.operations import sudo
+
+GIT_REPO = "https://github.com/xusanpangzi/Blog.git"
+
+env.user = 'sanpangzi'
+env.password = 'xuyameng32386964'
+
+env.hosts = ['三胖子.xyz']
+
+env.port = '27681'
+
+def deploy():
+    source_folder = '/home/sanpangzi/sites/三胖子.xyz/Blog'
+
+    run('cd %s && git pull' % source_folder)
+    run("""
+       cd {} &&
+       ../env/bin/pip install -r requirements.txt &&
+       ../env/bin/python3 manage.py collectatic --noinput &&
+       ../env/bin/python3 manage.py migrate &&
+       ../env/bin/gunicorn --bind unix:/tmp/三胖子.xyz.socket Blog.wsgi:application&
+       """.format(source_folder))
+    sudo('service nginx reload')
